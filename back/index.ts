af3e8cfd2aef7ff3ffc9364e647e9bf5c6724460
nitkinjoaco <nitkinjoaco@gmail.com>
2026-09-01 15:53:34 -0300

    a

--- back/index.ts
+++ back/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {parsear} from "./parser.ts";
 import { crearMock } from "./serial/mock.ts";
 import {guardar, obtenerultima} from "./storage.ts"  ;
@@ -11,14 +10,3 @@ fuente.on("data", (linea: string) => {
     guardar(medicion);
     console.log("última en memoria →", obtenerultima());
 });
-=======
-import type { medidcion } from "./tipos.ts";
-import { crearMock } from "./serial/mock.ts";
-
-const emisor = crearMock();
-
-emisor.on("data", (linea: string) =>{
-    console.log(linea);
-});
-
->>>>>>> 8d09ffcdcce832e61ab5914e82753eecf364f076
