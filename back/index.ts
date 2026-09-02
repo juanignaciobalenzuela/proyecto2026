6e104704608447b13b144034ba84962534fd2174
nitkinjoaco <nitkinjoaco@gmail.com>
2026-09-02 14:55:18 -0300

    hice la ruta ya se abre un servidor

--- back/index.ts
+++ back/index.ts
@@ -1,6 +1,7 @@
 import {parsear} from "./parser.ts";
 import { crearMock } from "./serial/mock.ts";
-import {guardar, obtenerultima} from "./storage.ts"  ;
+import {guardar, obtenerUltima} from "./storage.ts"  ;
+import { crearServidor } from "./api/rutas.ts";
 
 const fuente = crearMock();
 
@@ -8,5 +9,7 @@ fuente.on("data", (linea: string) => {
     const medicion = parsear(linea);
     if (medicion === null) return;
     guardar(medicion);
-    console.log("última en memoria →", obtenerultima());
+    console.log("última en memoria →", obtenerUltima());
 });
+
+crearServidor (3000);
