1802f15bd35b7c12d486c711c6ffb3ef74d19cd5
juanignaciobalenzuela <juanibaldem4@gmail.com>
2026-09-25 14:21:50 -0300

    empeze con el js

---
+++ front/main.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector('.boton5');
+const cuadrado = document.querySelector('cuadrado12');
+
+boton.addEventListener('click', () => {
+    cuadrado.classList.toggle('mostrar'); 
+
+
+
+});
